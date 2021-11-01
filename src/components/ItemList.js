3042fa49fb41809460114106ecaaa39aa506892d
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-11-01 11:06:35 +0530

    checkbox and css added

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { Data } from "./Data";
-// import BasicTextFields from "./components/SearchBox";
+import BasicTextFields from "./components/SearchBox";
 import Items from "./Items";
+import Checkbox from "../CheckBox";
+import "./ItemList.css";
 
 const ItemList = () => {
   return (
-    <div>
-      <div>
+    <div className="first-container">
+      <div className="searchbox-container">
+        <BasicTextFields />
+        <Checkbox />
+      </div>
+      <div className="list-container">
         {Data.map((item) => {
-          return <Items name={item.name} price={item.price} />;
+          return (
+            <Items
+              name={item.name}
+              price={item.price}
+              category={item.category}
+            />
+          );
         })}
       </div>
     </div>
