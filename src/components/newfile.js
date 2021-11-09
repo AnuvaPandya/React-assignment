e76a86267157d46b1000591c4e13d6f90f5aba1f
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-11-09 15:39:21 +0530

    new change

--- src/components/newfile.js
+++
@@ -1,7 +0,0 @@
-const numbers = [10, 3, 6, 1, 9, 21];
-
-function maxMin() {
-  var max = Math.max(numbers);
-  var min = Math.min(numbers);
-  return console.log("max number " + max + "and min number " + min);
-}
